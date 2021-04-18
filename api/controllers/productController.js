7cc3aae2369c02fbd29af375e250e0ad3e093f62
neha3010 <44228494+neha3010@users.noreply.github.com>
2021-04-18 21:51:56 +0530

    Update productController.js

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -1,9 +1,9 @@
-// import Todo Model
+// import Product Model
 const  Product = require("../models/productModel");
 
 // DEFINE CONTROLLER FUNCTIONS
 
-// listAllTodos function - To list all todos
+// listAllProducts function - To list all products
 exports.listAllProducts = (req, res) => {
 Product.find({}, (err, product) => {
 if (err) {
@@ -13,7 +13,7 @@ res.status(200).json(product);
 });
 };
 
-// createNewTodo function - To create new todo
+// createNewProduct function - To create new product
 exports.createNewProduct = (req, res) => {
     console.log("request body",req.body);
     let  newTodo = new Product (req.body);
@@ -26,7 +26,7 @@ exports.createNewProduct = (req, res) => {
     });
 };
 
-// updateTodo function - To update todo status by id
+// updateProduct function - To update product status by id
 exports.updateProduct = (req, res) => {
 Product.findOneAndUpdate({ productId:req.params.id }, req.body, {new:true}, (err, product) => {
 if (err) {
@@ -36,7 +36,7 @@ res.status(200).json(product);
 });
 };
 
-// deleteTodo function - To delete todo by id
+// deleteProduct function - To delete product by id
 exports.deleteProduct = async ( req, res) => {
 await  Product.deleteOne({ productId: req.params.id}, (err) => {
 if (err) {
