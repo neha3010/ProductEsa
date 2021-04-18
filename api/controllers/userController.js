bddbcee3a88da1718b950690f864d36797f22c8f
neha3010 <44228494+neha3010@users.noreply.github.com>
2021-04-18 21:43:17 +0530

    Update userController.js

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -26,7 +26,7 @@ res.status(201).json(user);
 
 // updateTodo function - To update todo status by id
 exports.updateUser = (req, res) => {
-Cart.findOneAndUpdate({ _id:req.params.id }, req.body, { new:true }, (err, todo) => {
+Cart.findOneAndUpdate({ uuid:req.params.id }, req.body, { new:true }, (err, todo) => {
 if (err) {
 res.status(500).send(err);
 }
@@ -36,7 +36,7 @@ res.status(200).json(user);
 
 // deleteTodo function - To delete todo by id
 exports.deleteUser = async ( req, res) => {
-await  User.deleteOne({ _id:req.params.id }, (err) => {
+await  User.deleteOne({ uuid:req.params.id }, (err) => {
 if (err) {
 return res.status(404).send(err);
 }
